5c0ae192a96042a3ff62b5a1599e45353cbd0c5c
dannyid <danielshekhtman@gmail.com>
2015-06-24 02:46:22 -0400

    Store log variable locally and add log endpoint

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ var express = require('express'),
     http = require('http'),
     server = http.createServer(app),
     fs = require('fs'),
-    util = require('util');
+    util = require('util'),
+    log;
 
 app.use(express.static(__dirname + '/public'));
+
+app.get("/log", function(req, res){
+  res.send(log);
+});
 
 app.get("/:url", function(req, res){
   writeToLog(req, res).redirect('http://google.com');
@@ -20,15 +25,7 @@ app.get("/", function(req, res){
 });
 
 function writeToLog(req, res) {
-  var json = util.inspect(req, { showHidden: true, depth: 20 });
-  fs.unlink('public/log.txt', function (err) {
-    if (err) { console.log('No file to delete'); }
-
-    fs.appendFile('public/log.txt', json, function (err) {
-      if (err) { console.log('There was an error appending to the log'); }
-    });
-    console.log('Request written to log');
-  });
+  log = util.inspect(req, { showHidden: true, depth: 20 });
   console.log('Redirect logged!', new Date());
   return res;
 }
