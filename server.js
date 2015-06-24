49dc7a64d3fded6f9411fc7084752788cff47a8a
dannyid <danielshekhtman@gmail.com>
2015-06-24 02:36:32 -0400

    Changing to async version of fs.unlink

--- server.js
+++ server.js
@@ -21,9 +21,13 @@ app.get("/", function(req, res){
 
 function writeToLog(req, res) {
   var json = util.inspect(req, { showHidden: true, depth: 20 });
-  fs.unlinkSync('public/log.txt');
-  fs.appendFile('public/log.txt', json, function (err) {
-    if (err) { console.log('Something blew up:', err); }
+  fs.unlink('public/log.txt', function (err) {
+    if (err) { console.log('No file to delete'); }
+
+    fs.appendFile('public/log.txt', json, function (err) {
+      if (err) { console.log('There was an error appending to the log'); }
+    });
+    console.log('Request written to log');
   });
   console.log('Redirect logged!', new Date());
   return res;
